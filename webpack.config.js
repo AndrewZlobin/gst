144ed44b9c835fde46ddbd66048e9b3afe5e0059
Andrew Zlobin <a.zlobin@lms-service.ru>
2021-04-21 00:07:17 +0300

    new icons + webpack/composer settings

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ Encore
         from: './assets/fonts',
         to: 'fonts/[name].[ext]'
     })
+    .copyFiles({
+        from: './assets/img',
+        to: 'img/[name].[ext]'
+    })
     // public path used by the web server to access the output path
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
@@ -29,7 +33,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/landing.js')
+    .addEntry('app', './assets/app.js')
+    .addEntry('landing', './assets/landing.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
