37197e0204f30bb57e34ee697dfd7b6e7ef2103a
Andrew Zlobin <a.zlobin@lms-service.ru>
2021-04-21 00:10:01 +0300

    new entrypoint for landing page

--- assets/landing.js
+++ assets/landing.js
@@ -6,8 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/main.scss';
-// import './styles/main.scss';
+import './styles/landing.scss';
 
 // start the Stimulus application
 import './bootstrap';
