aa1314bdd36199e3ed381f3ceff6f724661591b5
Andrew Zlobin <a.zlobin@lms-service.ru>
2021-05-03 13:26:00 +0300

    start js logic and styles for map

--- assets/landing.js
+++ assets/landing.js
@@ -1,12 +1,50 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/landing.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+//For map
+import * as d3 from "d3";
+import * as Datamap from "datamaps/dist/datamaps.rus";
+
+console.log(require('./data/rus.topo.json'));
+
+const mapSettings = {
+    defaultFill: '#2D394F',
+    center: [100.0000, 65.0000],
+    scale: 400,
+    rotate: [-10, 0, 0],
+};
+
+let map = new Datamap({
+    element: document.getElementById('map-container'),
+    responsive: true,
+    scope: 'RUS',
+    geographyConfig: {
+        dataJson: require('./data/rus.topo.json')
+    },
+    fills: {
+        defaultFill: mapSettings.defaultFill,
+    },
+    setProjection: function (element) {
+        console.log(element.offsetWidth, element.offsetWidth / 2)
+        const projection = d3.geo.mercator()
+            .center(mapSettings.center)
+            .scale(mapSettings.scale)
+            .translate([element.offsetWidth / 2, element.offsetHeight / 2])
+            .rotate(mapSettings.rotate);
+        const path = d3.geo.path()
+            .projection(projection);
+
+        return {
+            path: path,
+            projection: projection
+        };
+    }
+});
+
+// Make map responsive on different screens
+window.addEventListener('resize', function () {
+    map.resize();
+})
