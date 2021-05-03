3affd93f8188118803f4de529e3f44f39d62a997
Andrew Zlobin <a.zlobin@lms-service.ru>
2021-05-03 14:50:08 +0300

    some fixes for map rendering, need data-attributes from back

--- assets/landing.js
+++ assets/landing.js
@@ -12,6 +12,7 @@ console.log(require('./data/rus.topo.json'));
 
 const mapSettings = {
     defaultFill: '#2D394F',
+    pointColor: '#E8AB10',
     center: [100.0000, 65.0000],
     scale: 400,
     rotate: [-10, 0, 0],
@@ -21,14 +22,31 @@ let map = new Datamap({
     element: document.getElementById('map-container'),
     responsive: true,
     scope: 'RUS',
-    geographyConfig: {
-        dataJson: require('./data/rus.topo.json')
-    },
     fills: {
         defaultFill: mapSettings.defaultFill,
+        ye100: '#E8AB10'
+    },
+    geographyConfig: {
+        dataJson: require('./data/rus.topo.json'),
+        popupOnHover: false,
+        highlightOnHover: false
+    },
+    bubblesConfig: {
+        borderWidth: 2,
+        borderColor: '#E8AB10',
+        radius: 8,
+        popupOnHover: true,
+        popupTemplate: function(geography, data) { // This function should just return a string
+            return `<div class="hoverinfo">
+                        <div class="map-popup d-flex flex-column">
+                            <div class="map-popup__img" style="background-image: url('${require('./img/landing_bg.png')}');"></div>
+                            <h1>${data.name}</h1>
+                            <span>${data.caption}</span>
+                        </div>
+                    </div>`;
+        },
     },
     setProjection: function (element) {
-        console.log(element.offsetWidth, element.offsetWidth / 2)
         const projection = d3.geo.mercator()
             .center(mapSettings.center)
             .scale(mapSettings.scale)
@@ -43,6 +61,45 @@ let map = new Datamap({
         };
     }
 });
+
+map.bubbles([
+    {
+        latitude: 59.909427,
+        longitude: 30.255771,
+        fillKey: 'ye100',
+        name: 'Двинская',
+        caption: 'г. Санкт-Петербург, ул. Двинская, д. 3, лит. А, оф. 408.',
+    },
+    {
+        latitude: 55.655709,
+        longitude: 37.551747,
+        fillKey: 'ye100',
+        name: 'На научном',
+        caption: 'г. Санкт-Петербург, ул. Двинская, д. 3, лит. А, оф. 408.',
+    }
+]);
+
+Datamap.prototype.updatePopup = function (element, d, options) {
+    var self = this;
+    element.on('mousemove', null);
+    element.on('mousemove', function() {
+        var position = d3.mouse(self.options.element);
+        d3.select(self.svg[0][0].parentNode).select('.datamaps-hoverover')
+            .style('top', ( (position[1] - 120)) + "px")// Half of block height
+            .html(function() {
+                var data = JSON.parse(element.attr('data-info'));
+                try {
+                    return options.popupTemplate(d, data);
+                } catch (e) {
+                    return "";
+                }
+            })
+            .style('left', ( (position[0] + 21)) + "px");
+    });
+    d3.select(self.svg[0][0].parentNode)
+        .select('.datamaps-hoverover')
+        .style('display', 'block');
+};
 
 // Make map responsive on different screens
 window.addEventListener('resize', function () {
