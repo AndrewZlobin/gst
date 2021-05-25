6b209e53cda9c7be467a5499d8c0c0654a2c0622
Andrew Zlobin <a.zlobin@lms-service.ru>
2021-05-26 00:55:04 +0300

    landing page block rendering updated

--- src/Service/BlocksForLandingPage.php
+++ src/Service/BlocksForLandingPage.php
@@ -9,12 +9,20 @@ class BlocksForLandingPage
     private NavbarWithBannerGenerator $navbar;
     private AdvantagesGenerator $advantages;
     private MapGenerator $map;
+    private FooterWithBlockGenerator $footer;
+    private ActivitiesGenerator $activities;
 
-    public function __construct(NavbarWithBannerGenerator $navbar, AdvantagesGenerator $advantages, MapGenerator $map)
+    public function __construct(NavbarWithBannerGenerator $navbar,
+                                AdvantagesGenerator $advantages,
+                                MapGenerator $map,
+                                FooterWithBlockGenerator $footer,
+                                ActivitiesGenerator $activities)
     {
         $this->navbar = $navbar;
         $this->advantages = $advantages;
         $this->map = $map;
+        $this->footer = $footer;
+        $this->activities = $activities;
     }
 
     /**
@@ -41,5 +49,19 @@ class BlocksForLandingPage
         return $this->map;
     }
 
+    /**
+     * @return FooterWithBlockGenerator
+     */
+    public function getFooter(): FooterWithBlockGenerator
+    {
+        return $this->footer;
+    }
 
+    /**
+     * @return ActivitiesGenerator
+     */
+    public function getActivities(): ActivitiesGenerator
+    {
+        return $this->activities;
+    }
 }
