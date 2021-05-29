31115797da932cb3b4dc4d54aed72c1fd40fe69c
Andrew Zlobin <a.zlobin@lms-service.ru>
2021-05-29 15:01:06 +0300

    skeleton for `contact us` form

---
+++ src/Controller/ContactUsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactUsType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactUsController extends AbstractController
+{
+    /**
+     * @Route("/contact/us", name="contact_us")
+     */
+    public function new(): Response
+    {
+        $form = $this->createForm(ContactUsType::class);
+
+        return $this->render('contact_us/form.html.twig', [
+            'controller_name' => 'ContactUsController',
+            'form' => $form->createView(),
+        ]);
+    }
+}
