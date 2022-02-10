ce6443837f307d57e643ac75c3a1b5b203b7bb8e
Andrew Zlobin <andrewzlobin1992@gmail.com>
2022-02-10 15:25:07 +0300

    Skeleton for Main activities page (navbar and footer blocks)

--- src/Controller/MainActivitiesController.php
+++ src/Controller/MainActivitiesController.php
@@ -5,20 +5,45 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+use App\Service\BlocksForActivitiesPage;
 
 class MainActivitiesController extends AbstractController
 {
+    const IDENTIFIER = 'main_activities';
+    const WRAPPER_CLASS = 'bg-custom-dark';
+
+    private TranslatorInterface $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     /**
-     * @Route("/main_activities/{type}", name="main_activities")
+     * @Route("/main_activities", name="main_activities")
      */
-    public function index(string $type = ''): Response
+    public function index(BlocksForActivitiesPage $blocks): Response
     {
 //        construction_and_installation
 //        design_and_survey
 //        complex_supplies
 //        construction_equipment_rental
+
+        $page = self::IDENTIFIER;
+
+        $navbar = $blocks->getNavbar();
+        $footer = $blocks->getFooter();
+
         return $this->render('main_activities/index.html.twig', [
-            'controller_name' => 'MainActivitiesController',
+            $navbar->getIdentifier() => $navbar->getNavbar(),
+            'pagetitle' => $this->translator->trans("pages.${page}"),
+            'extracontainerclasses' => self::WRAPPER_CLASS,
+            $footer->getIdentifier() => $footer->getFooter([
+                'footercontainer' => $this->translator->trans("pages.${page}"),
+                'extracontainerclasses' => 'bg-transparent'
+            ]),
         ]);
     }
 }
