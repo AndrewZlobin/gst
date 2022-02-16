555bf51530ef5a706ce47e5b306e159eaec28c7a
Andrew Zlobin <andrewzlobin1992@gmail.com>
2022-02-16 13:44:07 +0300

    data callback + fake routes for additional pages

--- src/Controller/MainActivitiesController.php
+++ src/Controller/MainActivitiesController.php
@@ -35,6 +35,7 @@ class MainActivitiesController extends AbstractController
 
         $navbar = $blocks->getNavbar();
         $footer = $blocks->getFooter();
+        $activitiesList = $blocks->getData();
 
         return $this->render('main_activities/index.html.twig', [
             'pagetitle' => $this->translator->trans("pages.${page}"),
@@ -44,6 +45,27 @@ class MainActivitiesController extends AbstractController
                 'footercontainer' => $this->translator->trans("pages.${page}"),
                 'extracontainerclasses' => 'bg-transparent'
             ]),
+            'activitieslist' => $activitiesList,
         ]);
     }
+
+    /**
+     * @Route("/main_activities/construction_and_installation", name="construction_and_installation")
+     */
+    public function constructionandinstallation() {}
+
+    /**
+     * @Route("/main_activities/design_and_survey", name="design_and_survey")
+     */
+    public function designandsurvey() {}
+
+    /**
+     * @Route("/main_activities/complex_supplies", name="complex_supplies")
+     */
+    public function complexsupplies() {}
+
+    /**
+     * @Route("/main_activities/construction_equipment_rental", name="construction_equipment_rental")
+     */
+    public function constructionequipmentrental() {}
 }
